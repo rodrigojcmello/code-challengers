e23272191d3457704ab1c8bbe15f40d3bb349df4
Rodrigo José Carvalho de Mello <rodrigojcmello@gmail.com>
2024-04-27 10:33:29 -0300

    feat: shape area challenger

--- challengers/codesignal/005-level-2-shape-area.test.ts
+++ challengers/codesignal/005-level-2-shape-area.test.ts
@@ -1,10 +1,10 @@
 import { shapeArea } from './005-level-2-shape-area';
 
 describe('shapeArea', () => {
-  it('should return the area of a polygon for a given number of sides', () => {
+  it('should return the area of a polygon for a given n', () => {
     expect(shapeArea(1)).toBe(1);
     expect(shapeArea(2)).toBe(5);
     expect(shapeArea(3)).toBe(13);
-    expect(shapeArea(4)).toBe(25);
+    expect(shapeArea(5)).toBe(41);
   });
 });
